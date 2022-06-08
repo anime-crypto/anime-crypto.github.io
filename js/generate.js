d8e28bb6f349960c32b8c0b49cf5ab7fee74c0be
anime-crypto <bekjan298@gmail.com>
2022-06-08 13:17:17 +0500

    created categories page

--- js/generate.js
+++ js/generate.js
@@ -1,8 +1,33 @@
+let animes;
+
 const xmlhttp = new XMLHttpRequest();
 xmlhttp.onload = function() {
-  const myObj = JSON.parse(this.responseText);
-  console.log(myObj);
-  document.getElementById("demo").innerHTML = myObj.name;
+	animes = JSON.parse(this.responseText);
 }
-xmlhttp.open("GET", "json_demo.txt");
+xmlhttp.open("GET", "../python/anime_list.json");
 xmlhttp.send();
+
+$(document).ready(function(){
+	$(".product__item__text").children("h5").html(function(i, origHTML){
+		if (i < animes.length)
+			return animes[i].title
+		return origHTML
+	});
+	$(".product__item").parent().attr("href", function(i, orig){
+		if (i < animes.length)
+			return "animes/" + animes[i].name + ".html"
+		return orig
+	})
+	$(".product__item__pic").attr("data-setbg", function(i, orig){
+		if (i < animes.length)
+			return animes[i].image_link
+		return orig
+	});
+	$(".product__item__pic").attr("style", function(i, orig){
+		if (i < animes.length)
+			return 'background-image: url("' + animes[i].image_link + '");'
+		return orig
+	});
+	$(".comment").remove();
+	$(".view").remove()
+});
