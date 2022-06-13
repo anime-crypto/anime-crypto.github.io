786aab36282eaa9e1ec82ebbb0eaa448640507cf
anime-crypto <bekjan298@gmail.com>
2022-06-13 12:37:31 +0500

    loads json after page load

--- js/generate.js
+++ js/generate.js
@@ -1,33 +1,32 @@
-let animes;
-
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onload = function() {
-	animes = JSON.parse(this.responseText);
-}
-xmlhttp.open("GET", "../python/anime_list.json");
-xmlhttp.send();
-
 $(document).ready(function(){
-	$(".product__item__text").children("h5").html(function(i, origHTML){
-		if (i < animes.length)
-			return animes[i].title
-		return origHTML
-	});
-	$(".product__item").parent().attr("href", function(i, orig){
-		if (i < animes.length)
-			return "animes/" + animes[i].name + ".html"
-		return orig
-	})
-	$(".product__item__pic").attr("data-setbg", function(i, orig){
-		if (i < animes.length)
-			return animes[i].image_link
-		return orig
-	});
-	$(".product__item__pic").attr("style", function(i, orig){
-		if (i < animes.length)
-			return 'background-image: url("' + animes[i].image_link + '");'
-		return orig
-	});
-	$(".comment").remove();
-	$(".view").remove()
+	let animes;
+
+	const xmlhttp = new XMLHttpRequest();
+	xmlhttp.onload = function() {
+		animes = JSON.parse(this.responseText);
+		$(".product__item__text").children("h5").html(function(i, origHTML){
+			if (i < animes.length)
+				return animes[i].title
+			return origHTML
+		});
+		$(".product__item").parent().attr("href", function(i, orig){
+			if (i < animes.length)
+				return "animes/" + animes[i].name + ".html"
+			return orig
+		})
+		$(".product__item__pic").attr("data-setbg", function(i, orig){
+			if (i < animes.length)
+				return animes[i].image_link
+			return orig
+		});
+		$(".product__item__pic").attr("style", function(i, orig){
+			if (i < animes.length)
+				return 'background-image: url("' + animes[i].image_link + '");'
+			return orig
+		});
+		$(".comment").remove();
+		$(".view").remove()
+	}
+	xmlhttp.open("GET", "../python/anime_list.json");
+	xmlhttp.send();
 });
