c47ca5b42110211bb61785d2360d1ff479017818
anime-crypto <bekjan298@gmail.com>
2022-06-16 17:16:33 +0500

    added series

--- python/generate.py
+++ python/generate.py
@@ -1,25 +1,17 @@
 import json
 
-def init_animes(animes):
-    with open("../animes/template.html", "r") as file:
+def init(items, path):
+    with open(path + "/template.html", "r") as file:
         template = file.read()
-    for anime in animes:
+    for item in items:
         page = template
-        page = page.format(**anime)
-        with open(f"../animes/{anime['name']}.html", "w") as out:
-            out.write(page)
-
-def init_films(films):
-    with open("../films/template.html", "r") as file:
-        template = file.read()
-    for film in films:
-        page = template
-        page = page.format(**film)
-        with open(f"../films/{film['name']}.html", "w") as out:
+        page = page.format(**item)
+        with open(f"{path}/{item['name']}.html", "w") as out:
             out.write(page)
 
 # print(template)
 with open("list.json", "r") as json_file:
     list = json.loads(json_file.read())
-init_animes(list["animes"])
-init_films(list["films"])
+init(list["animes"], "../animes")
+init(list["films"], "../films")
+init(list["series"], "../series")
